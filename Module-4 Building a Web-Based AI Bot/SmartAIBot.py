da27dc0f2bea34cacf42e6903e2b6e1c840bf170
Tabrez Ahmed <tabrezhere@gmail.com>
2026-09-18 23:19:20 +0530

    Update SmartAIBot.py

--- Module-4 Building a Web-Based AI Bot/SmartAIBot.py
+++ Module-4 Building a Web-Based AI Bot/SmartAIBot.py
@@ -1,14 +1,35 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from openai import OpenAI
-import os
 
 app = FastAPI()
 
+# ==============================
+# CORS
+# ==============================
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# ==============================
 # Azure AI / OpenAI client
+# ==============================
+
 client = OpenAI(
     api_key="",
     base_url=""
 )
+
+
+# ==============================
+# ASK API
+# ==============================
 
 @app.get("/ask")
 def ask(question: str):
